51102c109a6462b984ee275d417ae6fb926c6aa5
zeeshan <zeeshanjoiya4@gmail.com>
2020-12-28 15:27:56 +0500

    worked on additon calculation feature in calculator

---
+++ js/index.js
@@ -0,0 +1,43 @@
+/* Addition
+let  x,y,z;
+x = 5
+y = 6
+z = x + y ;
+console.log(z)
+*/
+/* Substraction
+let x,y,z
+x = 50
+y = 30
+z = x-y
+console.log(z)
+*/
+ function addition(a,b){
+     c = a+b;
+     return c;
+ }
+ function substract(d,e){
+     f = d-e;
+     return f ;
+ }
+
+ function multiplication(g,h){
+     i = g*h;
+     return i ;
+ }
+ function divide(j,k){
+     l = j/k;
+     return l ;
+ }
+
+ let add = addition (5,10)
+ let sub = substract(10,5)
+ let mul = multiplication(10,5)
+ let div = divide(50,25)
+
+
+console.log(add)
+console.log(sub)
+console.log(mul)
+console.log(div)
+
